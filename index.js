a8d4a416084f91434869cac856cd01e4413ff01c
menya <79504406065@ya.ru>
2017-03-04 01:42:59 +0500

    heroku webhook

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const app         = express();
 const TelegramBot = require('node-telegram-bot-api');
-const CONF = process.env;
+const CONF        = process.env;
+const PORT        = CONF.PORT || 5000;
 
-const bot = new TelegramBot(CONF.TOKEN, {polling: true});
+app.use(bodyParser.json());
+
+const server = app.listen(PORT, "0.0.0.0", function() {
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log('Web server started at http://%s:%s', host, port);
+});
+
+app.post('/bot', function(req, res) {
+	bot.processUpdate(req.body);
+	res.sendStatus(200);
+});
+
+const bot = new TelegramBot(CONF.TOKEN);
+bot.setWebHook(process.env.HEROKU_URL + 'bot');
 
 bot.on('message', (msg) => {
 	const chatId = msg.chat.id;
