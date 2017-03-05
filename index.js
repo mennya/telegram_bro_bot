693c21b210e1b22f283c8d8524286ffc439c8cb7
menya <79504406065@ya.ru>
2017-03-05 20:49:50 +0500

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const app         = express();
-const TelegramBot = require('node-telegram-bot-api');
-const CONF        = process.env;
-const PORT        = CONF.PORT || 5000;
-
+const express    = require('express');
+const bodyParser = require('body-parser');
+const CONF       = require('./config');
+const app        = express();
+const bot        = new (require('./src/bot'))(CONF);
 app.use(bodyParser.json());
 
-console.log(process.env);
-
-const server = app.listen(PORT, '0.0.0.0', function() {
+const server = app.listen(CONF.PORT, '0.0.0.0', () => {
 	const host = server.address().address;
 	const port = server.address().port;
 	console.log('Web server started at http://%s:%s', host, port);
@@ -20,44 +16,14 @@ app.post('/bot', function(req, res) {
 	res.sendStatus(200);
 });
 
-app.get('/bot', function(req, res) {
-	bot.sendMessage(-1001110145498, req.query.sendText);
+app.get('/bot', (req, res) => {
+	let text = req.query.sendText;
+
+	if (!text) {
+		return res.send(`The "sendText" parameter should be provided. Message not sent!`);
+	}
+
+	bot.bot.sendMessage(CONF.BRO_GROUP_ID, req.query.sendText);
 
 	res.send(`OK! The message "${req.query.sendText}" has been sent to Bro Group.`);
 });
-
-const bot = new TelegramBot(CONF.TOKEN);
-bot.setWebHook(process.env.HEROKU_URL + 'bot');
-
-bot.on('message', (msg) => {
-	const chatId = msg.chat.id;
-
-	console.log('new', msg);
-
-	if (msg.text) {
-		if (msg.text.match(/‼️/i)) {
-			bot.sendVideo(chatId, 'http://i.giphy.com/ntts0sjEqNaRG.gif', {reply_to_message_id: msg.message_id});
-		}
-
-		if (checkForMatch(msg.text, ['миша', 'мишка', 'darmy', 'misha'])) {
-			bot.sendVideo(chatId, 'http://i.giphy.com/SWCKeoosjekyQ.gif', {reply_to_message_id: msg.message_id});
-		}
-
-		if (checkForMatch(msg.text, ['bro', 'бро'])) {
-			bot.sendVideo(chatId, 'http://i.giphy.com/Tv2btKgK06tPy.gif', {reply_to_message_id: msg.message_id});
-		}
-
-		if (checkForMatch(msg.text, ['ахаха'])) {
-			bot.sendVideo(chatId, 'http://i.giphy.com/tqf2j43xoEcow.gif', {reply_to_message_id: msg.message_id});
-		}
-	}
-});
-
-function checkForMatch(string, array) {
-	const str = string.toLowerCase();
-	for (let i = 0; i < array.length; i++) {
-		if (str === array[i] || new RegExp('(?:^|\\s)' + array[i] + '(?:\\?|\\!|$|\\s)', 'gi').test(str)) {
-			return true;
-		}
-	}
-}
---
+++ src/bot.js
@@ -0,0 +1,47 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+module.exports = class {
+	constructor(CONF) {
+		if (process.env.NODE_ENV === 'development') {
+			this.bot = new TelegramBot(CONF.BOT_TOKEN, {polling: true});
+		} else {
+			this.bot = new TelegramBot(CONF.BOT_TOKEN);
+			this.bot.setWebHook(process.env.HEROKU_URL + 'bot');
+		}
+
+		this.bot.on('message', (msg) => {
+			console.log('new', msg);
+
+			if (msg.text) {
+				if (msg.text.match(/‼️/i)) {
+					this.sendGif(msg, 'ntts0sjEqNaRG');
+				}
+
+				if (this.checkForMatch(msg.text, ['миша', 'мишка', 'darmy', 'misha'])) {
+					this.sendGif(msg, 'SWCKeoosjekyQ');
+				}
+
+				if (this.checkForMatch(msg.text, ['bro', 'бро'])) {
+					this.sendGif(msg, 'Tv2btKgK06tPy');
+				}
+
+				if (this.checkForMatch(msg.text, ['ахаха'])) {
+					this.sendGif(msg, 'tqf2j43xoEcow');
+				}
+			}
+		});
+	}
+
+	checkForMatch(string, array) {
+		const str = string.toLowerCase();
+		for (let i = 0; i < array.length; i++) {
+			if (str === array[i] || new RegExp('(?:^|\\s)' + array[i] + '(?:\\?|\\!|$|\\s)', 'gi').test(str)) {
+				return true;
+			}
+		}
+	}
+
+	sendGif(msg, gifId) {
+		this.bot.sendVideo(msg.chat.id, `http://i.giphy.com/${gifId}.gif`, {reply_to_message_id: msg.message_id});
+	}
+};
