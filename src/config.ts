7f715fa2b15a094fba27104cccfb74b5d2e681dc
menya <79504406065@ya.ru>
2020-11-13 20:40:58 +0500

    migrate to dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,36 +1,10 @@
-import * as fs from 'fs';
+const dotenv = require('dotenv');
+dotenv.config({allowEmptyValues: true});
 
-interface IConf {
-  /**
-   * token from BotFather https://telegram.me/botfather
-   */
-  BOT_TOKEN: string;
-  /**
-   * An url to connect database
-   */
-  MONGOLAB_URI: string;
-  /**
-   * Port to run http server locally, heroku set's PORT env variable automatically
-   */
-  PORT: string;
-  /**
-   * Users id's who can edit answers
-   */
-  ADMINS: string;
-  /**
-   * SUPER ADMIN ID
-   */
-  SUPER_ADMIN: string;
-}
-
-let CONF;
-
-if (process.env.NODE_ENV === 'production') {
-  CONF = process.env;
-} else {
-  CONF = fs.readFileSync('./config.json', 'UTF-8');
-  CONF = JSON.parse(CONF);
-  // process.env.DEBUG = 'node-telegram-bot-api';
-}
-
-export const CONFIG: IConf = CONF;
+export const CONFIG = {
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  MONGOLAB_URI: process.env.MONGOLAB_URI,
+  PORT: process.env.PORT,
+  ADMINS: process.env.ADMINS,
+  SUPER_ADMIN: process.env.SUPER_ADMIN
+};
